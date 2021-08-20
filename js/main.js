ca4035d3724d559464e4e2a6d5d69f2ec397eeab
AlandraJh <44971878+AlandraJh@users.noreply.github.com>
2021-08-20 23:45:05 +1000

    Дабавлено модальное окно

--- js/main.js
+++ js/main.js
@@ -3,13 +3,26 @@ $(document).ready(function () {
     var counterUp = $(".counter-up"); /*Кнопка увелечения этажа*/ 
     var counterDown = $(".counter-down");/*Кнопка уменьшения этажа*/ 
     var floorPath = $(".home-image path"); /*Переменная подцветки этажей*/
+    var modal = $(".modal");
+    var modalDialog = $(".modal-dialog");
+    var closeBtn = $(".modal-close-btn");
+    var showFlatBtn =$(".show-flat-btn");
+
     /*События при наведении мыши на этаж*/
     floorPath.on("mouseover", function(){
         floorPath.removeClass("current-floor"); /*Удаление активного класса*/
         currentFloor = $(this).attr("data-floor"); /*Получение текушего этажа*/
         $(".counter").text(currentFloor); /*Запись в счетчик*/
     });
-    /*Событие при клике на верхнюю кнопку*/
+
+    /*Открытие-закрытие окна при клике на этаж*/
+    floorPath.on("click", toggleModal);
+
+ /*Открытие-закрытие окна при клике на кнопку "Показать квартиры"*/
+        showFlatBtn.on("click",toggleModal); 
+        closeBtn.on("click",toggleModal);
+
+ /*Событие при клике на верхнюю кнопку*/
     counterUp.on("click", function(){
         if (currentFloor < 18) {
             currentFloor++; /*Увеличение счетчика*/
@@ -31,6 +44,33 @@ $(document).ready(function () {
             
         }   
     });
+// Функция открытия-закрытия окна
+    function toggleModal () {
+        modal.toggleClass("modal-show");
+        modalDialog.toggleClass("modal-dialog-show");
+    }
 
+// // Подцветка карты квартир (доделать потом)
+// var currentFlat = 1 /*Переменная с текущей квартирой*/
+// var flatPath = $(".flat-image path"); /*Переменная подцветки квартир*/
+// var flatPathItem = $(".flat-link"); /*Ссылка на квартиру*/
+
+
+// /*События при наведении мыши на квартиру*/
+// flatPath.on("mouseover", function(){
+//     currentFlat = $(this).attr("data-flat"); /*Получение текушей квартиры*/
+//     flatPath.removeClass("current-flat"); /*Удаление активного класса */
+//     flatPathItem.removeClass("current-flat-item"); /*Удаление класса со ссылок с харектеристиками*/
+//     $(`[data-flat=${currentFlat}]`).toggleClass("current-flat"); /*Присвоение класса квартире*/
+//     $(`[data-flat=${currentFlat}]`).toggleClass("current-flat-item"); /*Присвоение класса*/
+// });
+
+//     /*События при наведении мыши на ссылки квартир */
+//     FlatLink.on("mouseover", function(){
+//         flatPath.removeClass("current-flat"); /*Удаление активного класса квартиры*/
+//         currentFlat = $(this).attr("data-flat");
+//         $(`[data-flat=${currentFlat}]`).toggleClass("current-flat"); /*Присвоение класса*/
+
+//     });
 
 });
